5188cf4c52004440a5a2d3a51c1fa9398272c0d7
Lin Dong <jpiilin+github+ldong@gmail.com>
2015-03-07 14:52:38 -0800

    Add bodyParser for express

---
+++ README.md
@@ -0,0 +1,18 @@
+# README
+
+Author: Lin Dong
+
+Date: Sat Mar  7 13:28:07 PST 2015
+
+## npm packages
+
+1. express
+2. nodemon
+3. jade
+4. ejs
+
+Express expects every view is in `./views` directory
+
+Express uses `render` to partial render html template
+
+## Verb
--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
     app = express();
 
-app.get('/', function(req, res){
-  res.send('<h1>hello world</h1>');
-}).listen(3000, function(){
-  console.log('listening on port 3000');
+app.use(bodyParser.urlencoded({ extended: false }));
 
+var names = ['John Doe', 'Lin Dong', 'Jane Doe'];
+
+app.get('/', function(req, res){
+  // res.send('<h1>hello world</h1>');
+  // res.render('index.jade', {
+  //   title: 'title from server.js'
+  // })
+  // res.render('index.ejs', {
+  //   title: 'title from server.js'
+  // })
+
+  res.render('index.ejs', {names: names});
+});
+
+
+app.post('/', function(req, res){
+  names.push(req.body.name);
+  res.redirect('/');
+});
+
+app.listen(3000, function(){
+  console.log('listening on port 3000');
 });
